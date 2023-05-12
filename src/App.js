4a9672ae49a215a1ff78740b7948d0a984215543
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-05-12 17:41:29 +0200

    css and other fun stuff

--- src/App.js
+++ src/App.js
@@ -2,34 +2,33 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './index.css';
 import Spells from 'components/Spells';
+import Header from 'components/Header';
 import Search from './components/Search';
 import Characters from './components/Characters';
 
 export const App = () => {
   return (
-    <div className="firstpage">
-      <BrowserRouter>
-        <h1>Hello and welcome to the Harry Potter API!</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/characters">All Characters</Link>
-            </li>
-            <li>
-              <Link to="/spells">Spells</Link>
-            </li>
-            <li>
-              <Link to="/search">Search Characters</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" exact> </Route>
-          <Route path="/characters" element={<Characters />} />
-          <Route path="/spells" element={<Spells />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/characters">All Characters</Link>
+          </li>
+          <li>
+            <Link to="/spells">Spells</Link>
+          </li>
+          <li>
+            <Link to="/search">Search Characters</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" exact> </Route>
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/spells" element={<Spells />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,29 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Loading from 'components/Loading'; // Import the Loading component
+import Loading from 'components/Loading';
 
 const Characters = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [characters, setCharacters] = useState([]);
+  const [showPatronusInfo, setShowPatronusInfo] = useState(false);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://project-mongo-api-pb7rmnzmyq-lz.a.run.app/characters');
         setCharacters(response.data);
-        setIsLoading(false); // Set isLoading to false when the movies are loaded
+        setIsLoading(false);
       } catch (error) {
         console.error(error);
-        setIsLoading(false); // Also set isLoading to false on error
+        setIsLoading(false);
       }
     };
     fetchData();
   }, []);
 
+  const handlePatronusClick = (character) => {
+    setSelectedCharacter(character);
+    setShowPatronusInfo(true);
+  };
+
+  const handleCloseClick = () => {
+    setShowPatronusInfo(false);
+    setSelectedCharacter(null);
+  };
+
   return (
     <div>
       <h1>All Characters</h1>
-      {isLoading ? ( // Render the Loading component if isLoading is true
+      {isLoading ? (
         <Loading />
       ) : (
         <div className="characters-container">
@@ -34,9 +46,38 @@ const Characters = () => {
               <p className="character-info">Species: {character.Species}</p>
               <p className="character-info">Wand (Wood): {character['Wand (Wood)']}</p>
               <p className="character-info">Wand (Core): {character['Wand (Core)']}</p>
-              <p className="character-info">Patronus: {character.Patronus}</p>
+              <p className="character-info">
+                Patronus:{' '}
+                {character.Patronus ? (
+                  <span
+                    onClick={() => handlePatronusClick(character)}
+                    onKeyDown={() => {}}
+                    role="button"
+                    tabIndex={0}
+                    className="patronus">
+                    {character.Patronus}
+                  </span>
+                ) : (
+                  'none'
+                )}
+              </p>
             </div>
           ))}
+          {showPatronusInfo && selectedCharacter && (
+            <div className="patronus-info">
+              <p>
+                <strong>{selectedCharacter['Character Name']}</strong> has a{' '}
+                <strong>{selectedCharacter.Patronus}</strong> patronus,
+                which is a magical guardian in
+                the shape of an animal. In the Harry Potter series,
+                a Patronus is a spell that conjures a magical guardian,
+                used primarily to protect against dark creatures known as Dementors.
+                The Patronus takes the form of an animal that represents the casters personality,
+                and is a manifestation of their positive emotions and memories.
+              </p>
+              <button type="button" onClick={handleCloseClick}>Close</button>
+            </div>
+          )}
         </div>
       )}
     </div>
