b15cec2153719bf4fb4f9c6193f5532d269d091b
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-07-23 14:37:39 +0200

    added h2

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ const Header = () => {
   return (
     <header className="header">
       <h1>Hello and welcome to the Harry Potter API!</h1>
+      <h2>(have a little patience and click on the buttons a few times, 
+        the api-call takes a while to get started)</h2>
     </header>
   );
 }
